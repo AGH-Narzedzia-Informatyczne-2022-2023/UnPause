52b3336e028392b1da7ad5a6ccfbbe02e7860974
jakubwiechniak <wiechniak.j@gmail.com>
2022-11-24 22:57:17 +0100

    etst

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
-red = [1,0,0,1]
-green = [0,1,0,1]
-blue =  [0,0,1,1]
-purple = [1,0,1,1]
+red = [1, 0, 0, 1]
+green = [0, 1, 0, 1]
+blue = [0, 0, 1, 1]
+purple = [1, 0, 1, 1]
+
 
 class HBoxLayoutExample(App):
     def build(self):
         layout = BoxLayout(padding=10)
         colors = [red, green, blue, purple]
-
+        ez = True
         for i in range(5):
             btn = Button(text="Button #%s" % (i+1),
                          background_color=random.choice(colors)
@@ -22,6 +23,7 @@ class HBoxLayoutExample(App):
 
             layout.add_widget(btn)
         return layout
+
 
 if __name__ == "__main__":
     app = HBoxLayoutExample()
