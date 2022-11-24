2e373471edb066de2d21a2ed98817fcfc3e5ee44
jakubwiechniak <wiechniak.j@gmail.com>
2022-11-24 22:22:49 +0100

    team buttons

--- main.py
+++ main.py
@@ -5,23 +5,26 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 
-red = [1,0,0,1]
-green = [0,1,0,1]
-blue =  [0,0,1,1]
-purple = [1,0,1,1]
+red = [1, 0, 0, 1]
+green = [0, 1, 0, 1]
+blue = [0, 0, 1, 1]
+purple = [1, 0, 1, 1]
+
 
 class HBoxLayoutExample(App):
     def build(self):
         layout = BoxLayout(padding=10)
         colors = [red, green, blue, purple]
+        team = ["Michał", "Janek", "Damian", "Przemek", "Kajtek", "Kuba"]
 
-        for i in range(5):
-            btn = Button(text="Button #%s" % (i+1),
+        for i in team:
+            btn = Button(text="%s" % (i),
                          background_color=random.choice(colors)
                          )
 
             layout.add_widget(btn)
         return layout
+
 
 if __name__ == "__main__":
     app = HBoxLayoutExample()
